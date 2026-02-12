045e33f6762ab4c30f08f622cc0d56fb08e6bbad
Qida Wang <qida866@gmail.com>
2026-02-11 22:08:58 -0800

    rename project to TaskFlow

--- src/main/java/com/qida/jarvislite/TaskFlowApplication.java
+++ src/main/java/com/qida/jarvislite/TaskFlowApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JarvisLiteApplication {
+public class TaskFlowApplication {
     public static void main(String[] args) {
-        SpringApplication.run(JarvisLiteApplication.class, args);
+        SpringApplication.run(TaskFlowApplication.class, args);
     }
 }
